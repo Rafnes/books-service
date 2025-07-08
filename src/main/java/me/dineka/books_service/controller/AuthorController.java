49d4d57b7e7a421f0ba536b4826c2637951f828e
Rafnes <rafzograf@gmail.com>
2025-07-08 12:31:45 +0300

    refactor: Author.class: поле birth_year переименовано в birthYear

--- src/main/java/me/dineka/books_service/controller/AuthorController.java
+++ src/main/java/me/dineka/books_service/controller/AuthorController.java
@@ -1,5 +1,7 @@
 package me.dineka.books_service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.dineka.books_service.DTO.CreateAuthorDTO;
 import me.dineka.books_service.model.Author;
 import me.dineka.books_service.service.AuthorService;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/authors")
+@Tag(name = "Авторы", description = "Операции для работы с авторами")
 public class AuthorController {
     private final AuthorService authorService;
 
@@ -17,17 +20,20 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
+    @Operation(summary = "Добавить автора", description = "Добавление нового автора")
     @PostMapping
     public Author addAuthor(CreateAuthorDTO dto) {
         return authorService.addAuthor(dto);
     }
 
+    @Operation(summary = "Получить список всех авторов", description = "Получение списка всех авторов с пагинацией")
     @GetMapping
     public List<Author> getAllAuthors(@RequestParam int page,
                                       @RequestParam int size) {
         return authorService.getAllAuthors(PageRequest.of(page, size));
     }
 
+    @Operation(summary = "Получить автора по id", description = "Получение автора по id")
     @GetMapping("/{id}")
     public Author getAuthor(@PathVariable Long id) {
         return authorService.getAuthorById(id);
